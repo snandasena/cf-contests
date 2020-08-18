194ee1e45c13eaab9c3031ec6d8741a1754484cf
sajith <sajith@digitalxlabs.com>
2020-08-18 06:42:43 +0530

    Added a sample sol

--- gb10/c.cpp
+++ gb10/c.cpp
@@ -21,6 +21,21 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vector<int> v(n);
+        for (int &i: v) cin >> i;
+
+        ll ans = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            ans += max(0, v[i] - v[i + 1]);
+        }
+
+        cout << ans << "\n";
+    }
+
 
     return 0;
 }
