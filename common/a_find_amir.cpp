be9267910a5534a93668c269781629f570365ea0
sajith <sajith@digitalxlabs.com>
2020-09-02 09:11:20 +0530

    Added a new GD sample

---
+++ common/a_find_amir.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << (n - 1) / 2;
+    return 0;
+}
