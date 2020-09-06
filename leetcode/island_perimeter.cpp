88f084f9b42daec39984f35e43bfe728ac5a81b6
sajith <sajith@digitalxlabs.com>
2020-09-06 09:07:24 +0530

    Imoroved last sample

--- leetcode/island_perimeter.cpp
+++ leetcode/island_perimeter.cpp
@@ -24,3 +24,15 @@ public:
         return ans;
     }
 };
+
+int main() {
+
+    vector<vector<int>> grid = {{0, 1, 0, 0},
+                                {1, 1, 1, 0},
+                                {0, 1, 0, 0},
+                                {1, 1, 0, 0}};
+
+    Solution solution;
+    cout << solution.islandPerimeter(grid);
+    return 0;
+}
