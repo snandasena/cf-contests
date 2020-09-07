5812b7fa2b6b9c9688386ef53cc6744fcfaaf39a
sajith <sajith@digitalxlabs.com>
2020-09-07 13:13:20 +0530

    Solved new problems

---
+++ cf668/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vector<int> v(n);
+        for (int &i: v) cin >> i;
+        for (int i = n - 1; i >= 0; --i) cout << v[i] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
