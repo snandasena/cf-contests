4c4c0b20ad3da2161c2f50d04f2b1f7ef0b51195
sajith <sajith@digitalxlabs.com>
2020-09-08 19:04:57 +0530

    Solved another one

---
+++ hacker_rank/frequency_queries.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+
+string rtrim(const string &);
+
+vector<string> split(const string &);
+
+// Complete the freqQuery function below.
+vector<int> freqQuery(vector<vector<int>> queries) {
+    map<int, int> x, y;
+    vector<int> ans;
+    for (auto &q: queries) {
+        if (q[0] == 1) {
+            int k = x[q[1]];
+            if (k > 0) y[k]--;
+            x[q[1]]++;
+            y[k + 1]++;
+
+        } else if (q[0] == 2) {
+            int k = x[q[1]];
+            if (k > 0) {
+                x[q[1]]--;
+                y[k]--;
+
+                if (y[k - 1] > 0) y[k - 1]++;
+
+            }
+        } else {
+            if (y[q[1]]) ans.emplace_back(1);
+            else ans.emplace_back(0);
+        }
+    }
+
+    return ans;
+
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    vector<vector<int>> queries(q);
+
+    for (int i = 0; i < q; i++) {
+        queries[i].resize(2);
+
+        string queries_row_temp_temp;
+        getline(cin, queries_row_temp_temp);
+
+        vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
+
+        for (int j = 0; j < 2; j++) {
+            int queries_row_item = stoi(queries_row_temp[j]);
+
+            queries[i][j] = queries_row_item;
+        }
+    }
+
+    vector<int> ans = freqQuery(queries);
+
+    for (int i = 0; i < ans.size(); i++) {
+        fout << ans[i];
+
+        if (i != ans.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+            find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+            s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
