d9ffc863291f351f9bf906477fae9eaddfc38d2a
sajith <sajith@digitalxlabs.com>
2020-09-20 09:04:26 +0530

    Solved a new problem

--- cf671/a.cpp
+++ cf671/a.cpp
@@ -21,6 +21,29 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    int t, n;
+    string s;
+    cin >> t;
+    while (t--) {
+        cin >> n >> s;
+
+        bool e = false, o = false;
+        for (int i = 1; i <= n; ++i) {
+            if (i % 2 == 1) {
+                o |= ((s[i - 1] - '0') % 2 == 1);
+            } else {
+                e |= ((s[i - 1] - '0') % 2 == 0);
+            }
+
+        }
+
+        if (n % 2 == 1) {
+            cout << (o ? 1 : 2) << "\n";
+        } else {
+            cout << (e ? 2 : 1) << "\n";
+        }
+
+    }
 
     return 0;
 }
