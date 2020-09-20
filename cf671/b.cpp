0a5e687c3d9318050363a1acae7b107c8732f5fc
sajith <sajith@digitalxlabs.com>
2020-09-20 16:08:01 +0530

    Solved a problem

--- cf671/b.cpp
+++ cf671/b.cpp
@@ -16,10 +16,29 @@ ll lcm(ll a, ll b) {
     return a / gcd(a, b) * b;
 }
 
+ll cal(ll x) {
+    return x * (x + 1) / 2;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    int t;
+    ll n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int ans = 0;
+        for (int i = 1; cal((1ll << i) - 1) <= n; ++i) {
+            ans++;
+            n -= cal((1ll << i) - 1);
+        }
+
+        cout << ans << "\n";
+
+    }
 
 
     return 0;
