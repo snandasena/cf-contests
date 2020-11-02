4f4ce48ac519041cf25137cec12784e2c2f0b6af
sajith <sajith@digitalxlabs.com>
2020-11-02 12:15:31 +0530

    Solved a new LC problem

---
+++ leetcode/valid_parantheses.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (auto &c: s) {
+            if (c == '(' || c == '[' || c == '{') st.push(c);
+            else {
+                if (st.empty()) return false;
+
+                auto tc = st.top();
+                if (abs(int(c) - int(tc)) > 2) return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    auto *solution = new Solution();
+    string x;
+    cin >> x;
+    cout << solution->isValid(x);
+    return 0;
+}
