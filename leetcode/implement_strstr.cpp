abddd131a26fa38f4890bae9cd5f7fb6c7103c44
sajith <sajith@digitalxlabs.com>
2020-11-04 08:18:34 +0530

    Solved a new LC problem

---
+++ leetcode/implement_strstr.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+        else haystack.find(needle);
+
+    }
+};
