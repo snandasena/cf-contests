381f7b264fb6e312fd2dfa420518a2d4749e0ae3
sajith <sajith@digitalxlabs.com>
2020-11-07 13:09:18 +0530

    Solved a new HR problem

---
+++ hacker_rank/encryption.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the encryption function below.
+string encryption(string s) {
+    int len = static_cast<int>(s.length());
+    int col = static_cast<int>(ceil(sqrt(len)));
+    string ans;
+    for (int i = 0; i < col; ++i) {
+        for (int j = i; j < len; j += col) {
+            ans += s[j];
+        }
+        ans += ' ';
+    }
+
+    return ans;
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = encryption(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
