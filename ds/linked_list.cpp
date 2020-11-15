b3b842f64c0ea14339da5acbcc645f2c3463266e
sajith <sajith@digitalxlabs.com>
2020-11-15 12:56:42 +0530

    Added new linked list cpp sample

---
+++ ds/linked_list.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+struct Node
+{
+    int data;
+    Node *link;
+};
+
+void Insert(Node **, int);
+
+void Print(Node*);
+
+int main()
+{
+    // creating linked list head
+    Node *head = nullptr;
+    for (int i = 0; i < 10; ++i)
+    {
+        Insert(&head, i);
+        Print(head);
+    }
+    return 0;
+}
+
+void Insert(Node **head, int x)
+{
+    Node *temp = new Node();
+    temp->data = x;
+    temp->link = nullptr;
+
+    if (*head != nullptr) temp->link = *head;
+    *head = temp;
+}
+
+void Print(Node *head)
+{
+    Node *temp = head;
+    cout << "Start printing...\n";
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->link;
+    }
+
+    cout << "\n";
+}
